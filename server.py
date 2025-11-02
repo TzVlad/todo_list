9a53c56631328c374d63a0e53f025959300f9d34
TzVlad <tzenglevigh@gmail.com>
2025-11-02 22:34:44 +0200

    local database

--- server.py
+++ server.py
@@ -1,37 +1,52 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
 
-todos = []
-next_id = 1
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/todos', methods=['GET'])
 def get_todos():
-    return jsonify(todos)
+    todos = Todo.query.all()
+    return jsonify([{'id': t.id, 'title': t.title, 'completed': t.completed} for t in todos])
 
 @app.route('/todos', methods=['POST'])
 def create_todo():
-    global next_id
-    todo = {'id': next_id, 'title': request.json['title'], 'completed': False}
-    todos.append(todo)
-    next_id += 1
-    return jsonify(todo)
+    data = request.json
+    new_todo = Todo(title=data['title'])
+    db.session.add(new_todo)
+    db.session.commit()
+    return jsonify({'id': new_todo.id, 'title': new_todo.title, 'completed': new_todo.completed})
 
 @app.route('/todos/<int:id>', methods=['PUT'])
 def update_todo(id):
-    for todo in todos:
-        if todo['id'] == id:
-            todo['completed'] = request.json['completed']
-            return jsonify(todo)
+    todo = Todo.query.get(id)
+    if todo:
+        todo.completed = request.json['completed']
+        db.session.commit()
+        return jsonify({'id': todo.id, 'title': todo.title, 'completed': todo.completed})
     return jsonify({'error': 'Not found'}), 404
 
 @app.route('/todos/<int:id>', methods=['DELETE'])
 def delete_todo(id):
-    global todos
-    todos = [t for t in todos if t['id'] != id]
-    return jsonify({'deleted': id})
+    todo = Todo.query.get(id)
+    if todo:
+        db.session.delete(todo)
+        db.session.commit()
+        return jsonify({'deleted': id})
+    return jsonify({'error': 'Not found'}), 404
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
